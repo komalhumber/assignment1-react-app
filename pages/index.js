7e87a3438aef539804bdc255ab983666573e05dd
komalhumber <n01739705@humber.ca>
2025-06-03 22:45:01 -0400

    Styled homepage with welcome message and button

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
-import Link from 'next/link';
-
 export default function Home() {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Welcome to My React App</h1>
-      <p>This is the home page for Assignment 1</p>
-
-      <nav style={{ marginTop: '20px' }}>
-        <Link href="/about"><button>About</button></Link>
-        <Link href="/counter"><button>Counter</button></Link>
-      </nav>
+    <div className="container">
+      <h1>Welcome to My React Assignment!</h1>
+      <p>This is the homepage of your beautiful styled application. 😊</p>
+      <button onClick={() => alert("Hello, React!")}>Click Me</button>
     </div>
   );
 }
